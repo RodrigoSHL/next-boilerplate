5ef1771b54c9dc80dd8ef4e1814872dd7ca7f7fc
RodrigoSHL <shl.catalan@gmail.com>
2024-08-01 16:28:32 -0400

    init
    Signed-off-by: RodrigoSHL <shl.catalan@gmail.com>

---
+++ src/sections/Hero.tsx
@@ -0,0 +1,23 @@
+export const Hero = () => {
+  return (
+    <section>
+      <div className="container">
+        <div>
+          <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">
+            Version 2.0 is Here
+          </div>
+          <h1 className="text-5xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6">
+            Pathway to
+          </h1>
+          <p className="text-xl text-[#010D3E] tracking-tight mt-6">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quis quam aspernatur placeat, reiciendis doloremque harum repellat ad ut minus eos voluptates asperiores ipsa officiis nemo accusantium maiores mollitia vero.
+          </p>
+          <div className="flex gap-1 items-center mt-[30px]">
+            <button>Get for free</button>
+            <button>Learn more</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
