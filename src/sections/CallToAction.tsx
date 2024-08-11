f438d562fb32bb58aa3a17da37898012af9cbf6b
RodrigoSHL <shl.catalan@gmail.com>
2024-08-11 10:19:20 -0400

    FEAT: finish effect in call to action secton
    Signed-off-by: RodrigoSHL <shl.catalan@gmail.com>

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,28 +1,48 @@
+"use client";
+
 import ArrowRight from '@/assets/arrow-right.svg';
 import startImage from '@/assets/star.png'
 import springImage from '@/assets/spring.png'
 import Image from 'next/image';
+import { useRef } from 'react';
+import { useScroll, useTransform, motion } from 'framer-motion';
 
 export const CallToAction = () => {
+
+  const sectionRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"],
+  });
+
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150])
+
+
   return (
-    <section className='bg-gradient-to-b from-white to-[#D2DCFF] py-24 overflow-x-clip'>
+    <section ref={sectionRef}  className='bg-gradient-to-b from-white to-[#D2DCFF] py-24 overflow-x-clip'>
       <div className="container">
         <div className="section-heading relative">
           <h2 className="section-title mt-5">Sign up for free today</h2>
           <p className="section-description mt-5">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo, consequatur! Iure, aut perspiciatis. Voluptatibus similique corrupti quia aut, adipisci aperiam.
           </p>
-          <Image
-            src={startImage}
+          <motion.img
+            src={startImage.src}
             alt='Start Image'
             width={360}
             className='absolute -left-[350px] -top-[137px]'
+            style={{
+              translateY: translateY,
+            }}
           />
-          <Image
-            src={springImage}
+          <motion.img
+            src={springImage.src}
             alt='Start Image'
             width={360}
             className='absolute -right-[331px] -top-[19px]'
+            style={{
+              translateY: translateY,
+            }}
           />
           <div className="flex gap-2 justify-center mt-10">
             <button className="btn btn-primary">Get for free</button>
